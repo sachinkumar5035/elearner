403fe6ea4b8dda8effeaabc9b3426aa66afe5335
sachinkumar <sachinkumar5035@gmail.com>
2023-04-14 14:00:55 +0530

    home completed

--- elearner/src/component/Home/Home.jsx
+++ elearner/src/component/Home/Home.jsx
@@ -1,26 +1,87 @@
-import React from 'react'
-import { Stack } from "@chakra-ui/react";
-import './home.css'
+import React from 'react';
+import {
+    Box,
+    Button,
+    HStack,
+    Heading,
+    Image,
+    Stack,
+    Text,
+    VStack,
+} from '@chakra-ui/react';
+import './home.css';
+import { Link } from 'react-router-dom';
+import vg from '../../assets/images/bg.png';
+import { CgGoogle, CgYoutube } from "react-icons/cg";
+import { SiCoursera, SiUdemy, SiUdacity } from "react-icons/si";
+import { DiAws } from "react-icons/di";
+import introVideo from '../../assets/videos/intro.mp4';
+
 
 
 const Home = () => {
-  return (
-    <section className='home'>
-      <div className="container">
-        <Stack
-          direction={['column', 'row']}
-          height="100%"
-          justifyContent={['center','space-between']}
-          alignItems='center'
-          spacing={['16','56']}
-        >
-          <div>
-            This is home page
-          </div>
-        </Stack>
-      </div>
-    </section>
-  )
-}
+    return (
+        <section className="home">
+            <div className="container">
+                <Stack
+                    direction={['column', 'row']}
+                    height="100%"
+                    justifyContent={['center', 'space-between']}
+                    alignItems="center"
+                    spacing={['16', '56']}
+                >
+                    <VStack width={'full'} alignItems={['center', 'flex-end']}>
+                        <Heading children="Learn from the best teachers" size={'2xl'} />
+                        <Text children="find best courses at reasonable price" />
+                        <Link to="/courses">
+                            <Button size={'lg'} colorScheme="purple">
+                                Explore courses
+                            </Button>
+                        </Link>
+                    </VStack>
+                    <Image
+                        className='vector-graphics'
+                        boxsize={'md'}
+                        src={vg}
+                        objectFit={'contain'}
+                    />
+                </Stack>
+            </div>
+
+            <Box padding={'8'} bg={'blackAlpha.800'}>
+                <Heading textAlign={'center'}
+                    fontFamily={'body'}
+                    color={'purple.400'}
+                    children="OUR BRANDS"
+                />
+
+                <HStack className='brandsBanner' justifyContent={'space-evenly'} marginTop={'4px'}>
+                    <CgGoogle />
+                    <CgYoutube />
+                    <SiCoursera />
+                    <SiUdemy />
+                    <SiUdacity />
+                    <DiAws />
+                </HStack>  
+            </Box>
+
+            <div className="container2">
+                <video
+                    src={introVideo}
+                    autoPlay
+                    controls
+                    controlsList='nodownload nofullscreen noremoteplayback'
+                    disablePictureInPicture
+                    disableRemotePlayback
+                >
+
+                </video>
+            </div>
+
+
+
+        </section>
+    );
+};
 
 export default Home;
