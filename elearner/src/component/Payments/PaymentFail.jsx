a8f07e1f3d4ecd9e26f2f96bf2193d10da0811d1
sachinkumar <sachinkumar5035@gmail.com>
2023-04-26 23:29:46 +0530

    payment faild page added

--- elearner/src/component/Payments/PaymentFail.jsx
+++ elearner/src/component/Payments/PaymentFail.jsx
@@ -1,11 +1,21 @@
+import {  Button, Container, Heading, VStack } from '@chakra-ui/react'
 import React from 'react'
+import {  RiErrorWarningFill } from 'react-icons/ri'
+import { Link } from 'react-router-dom'
 
 const PaymentFail = () => {
   return (
-    <div>
-      PaymentFail
-    </div>
+    <Container height={'90vh'}>
+      <VStack justifyContent={'center'} height={'full'} spacing={'4'}>
+        <RiErrorWarningFill size={'5rem'}/>
+        <Heading textTransform={'uppercase'} children="Payment Failed"/>
+        <Link to={'/subscribe'}>
+            <Button colorScheme='purple'>Try again</Button>
+          </Link>
+      </VStack>
+    </Container>
   )
 }
+
 
 export default PaymentFail
