f84bd4fbfa0b0d4f0c06cfa1e3acc5fa6a004dc5
sachinkumar <sachinkumar5035@gmail.com>
2023-04-28 22:06:56 +0530

    change password page added

--- elearner/src/component/Profile/ChangePassword.jsx
+++ elearner/src/component/Profile/ChangePassword.jsx
@@ -1,12 +1,46 @@
-import { Container } from '@chakra-ui/react'
-import React from 'react'
+import { Button, Container, Heading, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const ChangePassword = () => {
-  return (
-    <Container paddingY={'16'}>
+    const [oldPassword,setOldPassword] = useState("");
+    const [newPassword,setNewPassword] = useState("");
+    return (
+        <Container paddingY={'16'} minH={'90vh'}>
+            <form>
+                <Heading
+                    textTransform={'uppercase'}
+                    children="Change password"
+                    textAlign={['center', 'left']}
+                    marginY={'16'}
+                />
+                    <VStack
+                        spacing={'8'}
+                    >
+                        <Input
+                            required
+                            id="oldPassword"
+                            value={oldPassword}
+                            onChange={e => setOldPassword(e.target.value)}
+                            placeholder="Old password"
+                            type="password"
+                            focusBorderColor="purple.400"
+                        ></Input>
+                        <Input
+                            required
+                            id="newPassword"
+                            value={newPassword}
+                            onChange={e => setNewPassword(e.target.value)}
+                            placeholder="New password"
+                            type="password"
+                            focusBorderColor="purple.400"
+                        ></Input>
+                        <Button width={'full'} colorScheme='purple' type='submit'>change</Button>
+                    </VStack>
 
-    </Container>
-  )
+                
+            </form>
+        </Container>
+    )
 }
 
 export default ChangePassword
