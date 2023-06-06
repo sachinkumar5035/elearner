542d45fd097f02565045b1c48a362f91b8adbc61
sachinkumar <sachinkumar5035@gmail.com>
2023-06-06 15:11:06 +0530

    added default route

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors'
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
@@ -12,6 +12,12 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended:true
 }))
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+    methods:["GET","POST","DELETE","PUT"]
+}))
+
 
 // to access cookie
 app.use(cookieParser()); 
@@ -23,6 +29,7 @@ import courseRoute from './routes/courseRoutes.js';
 import userRoute from './routes/userRoutes.js'
 import paymentRoute from "./routes/paymentRoutes.js"
 import otherRoute from './routes/otherRoutes.js';
+
 
 // importing middleware
 import ErrorMiddleware from './middlewares/error.js';
@@ -36,5 +43,6 @@ app.use('/api/v1',otherRoute);
 
 export default app;
 
+app.get("/",(req,res)=>res.send(`<h1>server is working.click<a href="${process.env.FRONTEND_URL}">here</a> to go to frontend</h1>`))
 
 app.use(ErrorMiddleware)
